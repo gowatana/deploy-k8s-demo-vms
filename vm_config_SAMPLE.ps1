106961891656c68cc2cd69456d45764c65f93b64
Go Watanabe <gowatana@vmtn.jp>
2020-01-18 01:37:41 +0900

    Add sample config

---
+++ vm_config_SAMPLE.ps1
@@ -0,0 +1,8 @@
+$lab_id = 3
+$template_vm = "ol76-min-01"
+$dc_name = "infra-dc-01"
+$folder_base = "vm"
+$folder_name = "ol7-k8s-lab-" + (0 + $lab_id).ToString("00")
+$rp_name = "rp-03-lab"
+$ds_name = "vsanDatastore"
+$pg_name = "vxw-dvs-30-virtualwire-14-sid-10003-ls-lab-k8s-003"
