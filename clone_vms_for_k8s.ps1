fd0d32bb5940faacc6f7a945952d7f8201885dc4
Go Watanabe <gowatana@vmtn.jp>
2020-01-18 03:39:48 +0900

    update

--- clone_vms_for_k8s.ps1
+++ clone_vms_for_k8s.ps1
@@ -1,33 +1,58 @@
 $vm_config_file = $args[0]
-
 Get-ChildItem $vm_config_file | Out-Null
 if(($? -eq $false) -or ($args.Count -lt 1)){"Config file not exists."; exit 1}
 . $vm_config_file
 
-Get-Folder -Type VM -Name $folder_base -ErrorAction:Stop
-$folder = Get-Datacenter -Name $dc_name | Get-Folder -Type VM -Name $folder_base | New-Folder -Name $folder_name 
+$lab_id_strings = $lab_id.ToString("00")
+Get-Folder -Type VM -Name $parent_folder_name -ErrorAction:Stop
+$folder = Get-Datacenter -Name $dc_name | Get-Folder -Type VM -Name $parent_folder_name |
+    New-Folder -Name $new_folder_name -ErrorAction:Stop
 
-("k8s-f-" + $lab_id.ToString() + "1"),
-("k8s-m-" + $lab_id.ToString() + "1"),
-("k8s-w-" + $lab_id.ToString() + "1"),
-("k8s-w-" + $lab_id.ToString() + "2"),
-("k8s-w-" + $lab_id.ToString() + "3")| % {
-    Get-VM $template_vm | New-VM -Name $_ `
-        -ResourcePool $rp_name `
-        -Location $folder `
+# Fumidai VMs
+$fumidai_vm_list = @()
+for($i = 1; $i -le $number_fumidai_vm; $i++){
+    $vm_number_strings = $i.ToString("00")
+    $vm_name_prefix = "k8s-f-"
+    $fumidai_vm_list += $vm_name_prefix + $lab_id_strings + "-" + $vm_number_strings
+}
+
+# Master VMs
+$master_vm_list = @()
+for($i = 1; $i -le $number_master_vm; $i++){
+    $vm_number_strings = $i.ToString("00")
+    $vm_name_prefix = "k8s-m-"
+    $master_vm_list += $vm_name_prefix + $lab_id_strings + "-" + $vm_number_strings
+}
+
+# Worker VMs
+$worker_vm_list = @()
+for($i = 1; $i -le $number_worker_vm; $i++){
+    $vm_number_strings = $i.ToString("00")
+    $vm_name_prefix = "k8s-w-"
+    $worker_vm_list += $vm_name_prefix + $lab_id_strings + "-" + $vm_number_strings
+}
+
+$vm_clone_tasks = @()
+$fumidai_vm_list + $master_vm_list + $worker_vm_list | ForEach-Object {
+    $vm_name = $_
+    "Clone VM: $vm_name"
+    $vm_clone_tasks += Get-VM $template_vm | New-VM -Name $vm_name `
+        -ResourcePool $rp_name -Location $folder `
         -Datastore $ds_name -StorageFormat Thin `
         -RunAsync
 }
 
+#Get-Task
+# | where {$_.Name -eq "CloneVM_Task"}
 $clone_wait_interval = 10
 $s = 0
-while((Get-Task | where {$_.Name -eq "CloneVM_Task"} | where {$_.State -ne "Success"}).Count -ne 0){
+while((Get-Task -Id ($vm_clone_tasks.Id) | where {$_.State -ne "Success"}).Count -ne 0){
     $s += $clone_wait_interval
     sleep $clone_wait_interval
-    echo ("clone wait: " + $s + "s")
+    Write-Host ("Clone wait: " + $s + "s")
 }
 
-$folder | Get-VM | Sort-Object Name | % {
+$folder | Get-VM | Sort-Object Name | ForEach-Object {
     $_ | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $pg_name -Confirm:$false | ft -AutoSize
     $_ | Set-VM -NumCpu 2 -MemoryGB 4 -Confirm:$false | ft -AutoSize
     $_ | New-AdvancedSetting -Name disk.EnableUUID -Value True -Confirm:$False | ft -AutoSize
@@ -35,8 +60,8 @@ $folder | Get-VM | Sort-Object Name | % {
     sleep 5
 }
 
-$vm_start_wait_sec = 60
-"VM Start wait: " + $vm_start_wait_sec + "s"
+$vm_start_wait_sec = 30
+Write-Host ("VM Start wait: " + $vm_start_wait_sec + "s")
 sleep $vm_start_wait_sec
 $folder | Get-VM | select `
     Name,
--- vm_config_SAMPLE.ps1
+++ vm_config_SAMPLE.ps1
@@ -1,8 +1,13 @@
-$lab_id = 3
+$lab_id = 5
+
+$number_master_vm = 1
+$number_worker_vm = 3
+$number_fumidai_vm = 1
+
 $template_vm = "ol76-min-01"
 $dc_name = "infra-dc-01"
-$folder_base = "vm"
-$folder_name = "ol7-k8s-lab-" + (0 + $lab_id).ToString("00")
+$parent_folder_name = "vm"
+$new_folder_name = "ol7-k8s-lab-" + (0 + $lab_id).ToString("00")
 $rp_name = "rp-03-lab"
 $ds_name = "vsanDatastore"
 $pg_name = "vxw-dvs-30-virtualwire-14-sid-10003-ls-lab-k8s-003"
